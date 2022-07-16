9df36b82e5172e8b38eb980cb984d7e8ba7f4623
Grzegorz Grzegrzółka <dzifors@Grzegorzs-iMac.local>
2022-07-16 23:56:41 +0200

    Added ExpressJS server setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { chats } = require("./data/data")
+
+const app = express()
+dotenv.config()
+
+app.get('/', (req, res) => {
+  res.send("Hello World!")
+})
+
+app.get('/api/chat', (req, res) => {
+  res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id)
+  res.send(singleChat)
+})
+
+const port = process.env.PORT || 5000
+
+app.listen(port, console.log(`Server started on port ${port}`))
