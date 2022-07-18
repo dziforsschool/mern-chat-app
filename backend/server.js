341371076dcd2a4dcb78f856ca70f0eb30b29ec5
Grzegorz Grzegrzółka <11121@kasprzak.edu.pl>
2022-07-18 22:12:56 +0200

    Connected MongoDB to the server

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,26 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const { chats } = require("./data/data")
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDatabase = require("./config/db");
+const colors = require("colors");
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
+connectDatabase();
 
-app.get('/', (req, res) => {
-  res.send("Hello World!")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/api/chat', (req, res) => {
-  res.send(chats)
-})
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
-app.get('/api/chat/:id', (req, res) => {
-  const singleChat = chats.find((c) => c._id === req.params.id)
-  res.send(singleChat)
-})
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, console.log(`Server started on port ${port}`))
+app.listen(port, console.log(`Server started on port ${port}`.yellow.bold));
