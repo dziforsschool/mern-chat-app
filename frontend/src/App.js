7c1a2fc2c7562f5906b05bf4528f4d3c0adf0b04
Grzegorz Grzegrzółka <11121@kasprzak.edu.pl>
2022-07-18 19:46:35 +0200

    Some unfinished UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import './App.css'
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Route } from 'react-router-dom'
@@ -7,8 +8,10 @@ import ChatPage from './Pages/ChatPage'
 function App() {
   return (
     <ChakraProvider>
-      <Route path='/' component={Homepage} exact />
-      <Route path='/chats' component={ChatPage} />
+      <div class="App">
+        <Route path='/' component={Homepage} exact />
+        <Route path='/chats' component={ChatPage} />
+      </div>
     </ChakraProvider>
   )
 }
--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,10 +1,22 @@
+import { Box, Container, Text } from "@chakra-ui/react";
 import React from "react";
 
 const Homepage = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Container maxW='xl' centerContent>
+      <Box
+      d='flex'
+      justifyContent="center"
+      p={3}
+      bg={'white'}
+      w="100%"
+      m="40px 0 15px 0"
+      borderRadius={"lg"}
+      borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Rubik">MERN Chat</Text>
+      </Box>
+    </Container>
   )
 }
 
